84d3d7410349270f717d5f8e8c8a03d0fbd55882
Ricky18code <rickyrawat2008@gmail.com>
2025-12-21 12:03:30 +0530

    Update validAnagram.cpp

--- sigma/strings/validAnagram.cpp
+++ sigma/strings/validAnagram.cpp
@@ -2,27 +2,25 @@
 #include <string>
 using namespace std;
 int main(){
-    string str1 = "The eyes";
-    string str2 = "They see";
+    string str1 = "car";
+    string str2 = "rat";
     int arr1[26]={0};
-    int arr2[26]={0};
     bool validAnagram = true;
     for(int i=0 ; i<str1.length() ; i++){
         arr1[str1[i]- 'a']++;
     }
     for(int i=0 ; i<str2.length() ; i++){
-        arr2[str2[i]- 'a']++;
-    }
-    for(int i=0 ; i<26 ; i++){
-        if(arr1[i] != arr2[i]){
+        if(--arr1[str2[i]-'a'] < 0){  //decrements the frequency in the same array to check wheter the character occur more or less time in the second string. Less than 0 means character occur more time in str2 and vice versa.
             validAnagram = false;
             break;
+        }else{
+            continue;
         }
     }
     if(validAnagram){
-        cout<<"ITS A VALID ANAGRAM"<<endl;
+        cout<<"VALID ANAGRAM";
     }else{
-        cout<<"NOT A VALID ANAGRAM"<<endl;
+        cout<<"NOT A VALID ANAGRAM";
     }
     return 0;
 }
